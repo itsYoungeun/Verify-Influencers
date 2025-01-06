f4c84387b007581bae1e9153b629f38b777b8eeb
Peter <youngeunpc@yahoo.com>
2025-01-06 12:23:16 -0300

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
@@ -22,7 +24,15 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api", influencerRoutes); // Use influencer routes here
 app.use("/api", claimRoutes);
-app.use("/api", researchRoutes)
+app.use("/api", researchRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   console.log('Server running on http://localhost:' + PORT);
